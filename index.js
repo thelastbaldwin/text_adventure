d147f74986447aa5bcf753b85a822b737cbc8a8b
Steve Minor <stv.mnr@gmail.com>
2017-08-24 19:11:10 -0700

    handling movement, sort of

--- index.js
+++ index.js
@@ -1,6 +1,83 @@
 const vorpal = require("vorpal")();
 const version = require("./package.json").version;
 
+const game = {
+    x: 0,
+    y: 0,
+    inventory: {}
+};
+
+function move(direction){
+   switch(direction){
+        case "east":
+        case "e":
+            game.x++;
+            break;
+        case "west":
+        case "w":
+            game.x--;
+            break;
+        case "south":
+        case "s":
+            game.y--;
+            break;
+        case "north":
+        case "n":
+            game.y++;
+            break;
+        default:
+            vorpal.log("You can't go that way");
+   }
+   vorpal.log(game);
+}
+
+const goNorth = move.bind(null, "north");
+const goSouth = move.bind(null, "south");
+const goEast = move.bind(null, "east");
+const goWest = move.bind(null, "west");
+
+vorpal
+    .command("e")
+    .alias("east")
+    .description("go east")
+    .action((args,callback) => {
+        goEast();
+        callback();
+    });
+
+vorpal
+    .command("w")
+    .alias("west")
+    .description("go west")
+    .action((args,callback) => {
+        goWest();
+        callback();
+    });
+vorpal
+    .command("n")
+    .alias("north")
+    .description("go north")
+    .action((args,callback) => {
+        goNorth();
+        callback();
+    });
+vorpal
+    .command("s")
+    .alias("south")
+    .description("go south")
+    .action((args,callback) => {
+        goSouth();
+        callback();
+    });
+vorpal
+    .command("go [direction]")
+    .description("move in a direction")
+    .alias("move")
+    .action((args, callback) => {
+        move(args.direction);
+        callback();
+    });
+
 vorpal
     .delimiter(">")
     .show();
